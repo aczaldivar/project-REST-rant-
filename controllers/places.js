9a74a2a134de0ff40e4d1d99c3a1f739d557db03
Anna Zaldivar <anna.zaldivar@gmail.com>
2023-04-28 07:13:54 -0700

    New places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-
+const places = require('../models/places.js')
 // GET /places
 router.get('/', (req,res)=>{
     let places = [{
@@ -27,11 +27,23 @@ router.get('/new', (req, res) => {
 
 //POST
 router.post('/', (req, res) => {
-  console.log(req.body)
-
-  res.send('POST/places')
+    console.log(req.body)
+    if (!req.body.pic) {
+      // Default image if one is not provided
+      req.body.pic = 'http://placekitten.com/400/400'
+    }
+    if (!req.body.city) {
+      req.body.city = 'Anytown'
+    }
+    if (!req.body.state) {
+      req.body.state = 'USA'
+    }
+    places.push(req.body)
+    res.redirect('/places')
+  });
+  
  
-});
+
 
 
 module.exports = router
