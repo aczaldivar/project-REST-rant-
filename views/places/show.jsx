aefa95ec289173a6e96530c5a331a72a30dc868d
Anna Zaldivar <anna.zaldivar@gmail.com>
2023-05-09 12:32:15 -0700

    comments attempt

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,7 +1,7 @@
 const React = require('react')
 const Def = require('../default')
 
-function show (data,place) {
+function show (data) {
     return (
         <Def>
           <main>
@@ -24,6 +24,38 @@ function show (data,place) {
             <button type="submit" className="btn btn-danger">
            Delete
             </button>
+     
+    let comments = (
+      <h3 className="inactive">
+        No comments yet!
+      </h3>
+    )
+    if (data.place.comments.length) {
+      comments = data.place.comments.map(c => {
+        return (
+          <div className="border">
+            <h2 className="rant">{c.rant ? 'Rant! ðŸ˜¡' : 'Rave! ðŸ˜»'}</h2>
+            <h4>{c.content}</h4>
+            <h3>
+              <stong>- {c.author}</stong>
+            </h3>
+            <h4>Rating: {c.stars}</h4>
+          </div>
+        )
+      })
+    }
+    return (
+        <Def>
+          <main>
+            <div className="row">
+              ...
+            </div>
+            <hr />
+            <h2>Comments</h2>
+            {comments}
+          </main>
+        </Def>
+    )
             </form>     
           </main>
         </Def>
